d183501a8e2693284e94a3efb4ef0bb7acbedca7
ThovenhairWorrior <wswm2007@naver.com>
2023-03-11 23:52:40 +0900

    3-11TIL

--- pr2023-3-11/3-1-review.js
+++ pr2023-3-11/3-1-review.js
@@ -34,3 +34,15 @@ function solution(phone_number) {
 // 레벨 0 : 팩토리얼
 // i팩토리얼 (i!)은 1부터 i까지 정수의 곱을 의미합니다. 예를들어 5! = 5 * 4 * 3 * 2 * 1 = 120 입니다. 정수 n이 주어질 때 다음 조건을 만족하는 가장 큰 정수 i를 return 하도록 solution 함수를 완성해주세요.
 
+function solution(n) {
+    var answer;
+    let Num = 1;
+    for (let i = 1; i <= n ; i++){
+        Num = Num * i;
+        if (Num > n) {
+            answer = i -1;
+            break;
+        } 
+    }
+    return answer;
+}
